ff52689d5dd5d8a9226d148cee4478a45c0e196b
Adam Romeo <adam.r.romeo@gmail.com>
2019-06-11 12:26:05 -0700

    Creates Link controller class

---
+++ ShrtLnks/Controllers/LinkController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShrtLnks.Data;
+
+namespace ShrtLnks.Controllers
+{
+    public class LinkController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public LinkController(
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}
