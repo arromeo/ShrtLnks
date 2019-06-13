0cb511df589763cf03c042a984ad76d5c64f4a28
Adam Romeo <adam.r.romeo@gmail.com>
2019-06-13 10:19:54 -0700

    Adds copy button and script for fast clipboarding

--- ShrtLnks/wwwroot/js/site.js
+++ ShrtLnks/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const links = document.getElementsByClassName("copy-link");
+
+if (links.length > 0) {
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function (event) {
+            const shortUrl = event.target.id;
+            const el = document.createElement('textarea');
+            el.value = 'https://localhost:44349/' + shortUrl;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand("copy");
+            document.body.removeChild(el);
+        });
+    }
+}
