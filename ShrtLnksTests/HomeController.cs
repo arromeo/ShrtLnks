b6a1ee2206998e329d1da88579059c7c641d9b72
Adam Romeo <adam.r.romeo@gmail.com>
2019-06-27 11:52:25 -0700

    Add object representation of DbContext

--- ShrtLnksTests/HomeController.cs
+++ ShrtLnksTests/HomeController.cs
@@ -5,6 +5,9 @@ using ShrtLnks.Controllers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ShrtLnks.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ShrtLnksTests
 {
@@ -24,6 +27,27 @@ namespace ShrtLnksTests
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+
+        private static List<Link> GetTestLinks()
+        {
+            var links = new List<Link>();
+            links.Add(new Link()
+            {
+                OwnerId = "abcdefg",
+                LongUrl = "http://example.com",
+                ShortUrl = "b3fSsm9",
+                CreateAt = new DateTime(2019, 1, 1)
+            });
+            links.Add(new Link()
+            {
+                OwnerId = "Anonymous",
+                LongUrl = "http://google.com",
+                ShortUrl = "asdfad9",
+                CreateAt = new DateTime(2019, 1, 2)
+            });
+
+            return links;
         }
     }
 }
