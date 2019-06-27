264a8cf988a7bb89d451c2650fb943184c6e9e78
Adam Romeo <adam.r.romeo@gmail.com>
2019-06-26 17:28:02 -0700

    Add test for Index action of Home controller

---
+++ ShrtLnksTests/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using Moq;
+using ShrtLnks.Data;
+using ShrtLnks.Controllers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShrtLnksTests
+{
+    public class TheHomeController
+    {
+        [Fact]
+        public void IndexReturnsView()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var controller = new HomeController(_dbContext);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
